e3a05e50167874cc70bf90f139e4cd32c76bd722
RAJEENTHINI <srajeenthini@gmail.com>
2015-01-05 22:13:18 +0530

    core added updated

--- 4.2.3/src/main/java/org/wso2/carbon/identity/core/dao/SAMLSSOMetadataIdentityProviderDAO.java
+++ 4.2.3/src/main/java/org/wso2/carbon/identity/core/dao/SAMLSSOMetadataIdentityProviderDAO.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2005,2006 WSO2, Inc. http://www.wso2.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.wso2.carbon.identity.core.dao;
 
 import org.apache.commons.codec.binary.Base64;
@@ -10,16 +26,13 @@ import org.wso2.carbon.registry.core.Resource;
 import org.wso2.carbon.registry.core.exceptions.RegistryException;
 import org.wso2.carbon.registry.core.jdbc.utils.Transaction;
 
-/**
- * Created by rajee on 12/31/14.
- */
-public class SAMLSSOMetadataIdentityProviderDAO extends AbstractDAO{
+
+public class SAMLSSOMetadataIdentityProviderDAO extends AbstractDAO {
     private static final Log log = LogFactory.getLog(SAMLSSOMetadataServiceProviderDAO.class);
 
     public SAMLSSOMetadataIdentityProviderDAO(Registry registry) {
         this.registry = registry;
     }
-
 
 
     /**
